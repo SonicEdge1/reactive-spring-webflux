81aa91c7b37b67b1366e14394176d4392fbecfba
joe bell <joseph.bell.17@us.af.mil>
2022-07-21 09:34:45 -0600

    Flux and Mono Generators and Tests complete

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,43 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("Joe", "Jennifer", "Alexa", "Wyatt", "Kyle", "Sadie"))
+                .log();
+    }
+    Flux<String> namesFluxUpperCase() {
+        return Flux.fromIterable(List.of("Joe", "Jennifer", "Alexa", "Wyatt", "Kyle", "Sadie"))
+                .map(String::toUpperCase);
+//                .map(s -> s.toUpperCase()) //same functionality as line above
+//                .log(); //.log shows all subscribe, onNext, and onComplete events in console
+    }
+
+    Flux<String> namesFluxStreamsAreImmutable() {
+        var reactiveStream = Flux.fromIterable(List.of("Joe", "Jennifer", "Alexa", "Wyatt", "Kyle", "Sadie"));
+        reactiveStream.map(String::toUpperCase);
+        return reactiveStream;
+//                .log(); //.log shows all subscribe, onNext, and onComplete events in console
+    }
+
+    Mono<String> namesMono() {
+        return Mono.just("Zeus");
+    }
+    public static void main(String[] args) {
+
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        var famNames = fluxAndMonoGeneratorService.namesFlux();
+        System.out.println("Family Names: ");
+        famNames.subscribe(name -> {
+            System.out.println(name);
+        });
+        fluxAndMonoGeneratorService.namesMono().subscribe(name -> {
+            System.out.println("the Mono name is: " + name);
+        });
+
+    }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,53 @@
 package com.learnreactiveprogramming.service;
 
+import org.junit.jupiter.api.Test;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FluxAndMonoGeneratorServiceTest {
 
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+    List<String> allNames = List.of("Jennifer", "Alexa", "Wyatt", "Kyle", "Sadie", "Joe");
+
+    @Test
+    void namesFluxUpperCase() {
+        var fluxUpperCaseNames = fluxAndMonoGeneratorService.namesFluxUpperCase();
+
+        StepVerifier.create(fluxUpperCaseNames)
+                .expectNext("JOE", "JENNIFER", "ALEXA", "WYATT", "KYLE", "SADIE")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFlux() {
+        var fluxNames = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(fluxNames)
+//                .expectNextSequence(allNames) //would expect an entire list to come through so won't work with 1 item
+//                .expectNextCount(6)
+                .expectNext("Joe", "Jennifer", "Alexa", "Wyatt", "Kyle")
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxStreamsAreImmutable() {
+        var fluxNames = fluxAndMonoGeneratorService.namesFlux();
+
+        StepVerifier.create(fluxNames)
+                .expectNext("Joe", "Jennifer", "Alexa", "Wyatt", "Kyle", "Sadie")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesMono() {
+        var monoNames = fluxAndMonoGeneratorService.namesMono();
+
+        StepVerifier.create(monoNames)
+                .expectNext("Zeus")
+                .verifyComplete();
+    }
 }
